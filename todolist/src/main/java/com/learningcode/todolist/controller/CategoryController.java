b3b413155d4fe77803385b5347756c95f0c3adb5
Pruthvi-Munshi <pruthvi.munshi@gmail.com>
2023-12-06 20:29:31 -0500

    controller setup & postman test

--- todolist/src/main/java/com/learningcode/todolist/controller/CategoryController.java
+++ todolist/src/main/java/com/learningcode/todolist/controller/CategoryController.java
@@ -1,4 +1,22 @@
 package com.learningcode.todolist.controller;
 
+import com.learningcode.todolist.model.Category;
+import com.learningcode.todolist.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @PostMapping("/add")
+    public String addCategory(@RequestBody Category category){
+        categoryService.createCategory(category);
+        return "Category was created";
+    }
 }
--- todolist/src/main/java/com/learningcode/todolist/controller/TaskController.java
+++ todolist/src/main/java/com/learningcode/todolist/controller/TaskController.java
@@ -1,4 +1,24 @@
 package com.learningcode.todolist.controller;
 
+// Contains classes annotated with @RestController that handle incoming HTTP requests
+
+import com.learningcode.todolist.model.Task;
+import com.learningcode.todolist.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/task")
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping("/add")
+    public String addTask(@RequestBody Task task){
+        taskService.createTask(task);
+        return "The task was added";
+    }
 }
