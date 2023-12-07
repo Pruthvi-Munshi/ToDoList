f2d7e516cf84b8a3eaa6bacc0bd3c1e3e029a55a
Pruthvi-Munshi <pruthvi.munshi@gmail.com>
2023-12-06 19:29:32 -0500

    model setup

--- todolist/src/main/java/com/learningcode/todolist/model/Category.java
+++ todolist/src/main/java/com/learningcode/todolist/model/Category.java
@@ -1,4 +1,61 @@
 package com.learningcode.todolist.model;
 
+// Contains classes representing data entities (e.g. JPA entities or DTOs)
+    // JPA (Java persistence API) entities are Java classes that represent objects in you application and are mapped to
+    //  database tables
+    // DTOs (Data transfer Objects) are objects used to transfer data between different layers of an application, such as
+    //  between the frontend and the backend
+
+// Spring Boot makes extensive use of annotations to simplify the configuration and development of Spring-based applications.
+// Annotations are markers added to Java classes or methods that provide additional information to the Spring framework.
+
+import jakarta.persistence.*;
+
+@Entity // Indicates that this class is a JPA entity and should be mapped to a database table.
+@Table(name="category")
 public class Category {
+    @Id
+    @Column(name="category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoryId;
+
+    @Column(name = "task_id")
+    private int taskId;
+
+    private String categoryName;
+
+    // GETTERS
+    public int getCategoryId(){
+        return categoryId;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public String getCategoryName(){
+        return categoryName;
+    }
+
+    // SETTERS
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", taskId=" + taskId +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
 }
--- todolist/src/main/java/com/learningcode/todolist/model/Task.java
+++ todolist/src/main/java/com/learningcode/todolist/model/Task.java
@@ -1,4 +1,57 @@
 package com.learningcode.todolist.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="task")
 public class Task {
+
+    @Id // like defining the primary key in SQL
+    @Column(name="task_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Like auto increment in SQL
+    private int taskId;
+
+    private String taskName;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    @JoinColumn(name="task_id")
+    private Set<Category> categories = new HashSet<>();
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
 }
